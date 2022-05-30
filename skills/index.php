5ef4efea6fc190e32b84e37530d0692347688ec1
DG-ogura <ogura@designet.co.jp>
2022-05-30 14:35:57 +0900

    add all form

---
+++ skills/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>スキルチェック</title>
+    <link rel="stylesheet" href="skill-check-app.css">
+</head>
+<body>
+    <div class="header">
+        <a href="../home/index.php">■ホーム</a>
+        <a href="../users/skill.php">&nbsp;■自分のスキル</a>
+        <a href="../users/skill_edit.php">&nbsp;■スキル点数変更</a>
+        <a>&nbsp;■スキル一覧</a>
+    </div>
+    <div class="top">
+        <h1 class="title">@スキルの一覧です@</h1>
+    </div>
+    <div class="errmsg">
+        <p><?php echo $errmsg; ?></p>
+    </div>
+    </html>
---
+++ users/skill_edit.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>スキルチェック</title>
+    <link rel="stylesheet" href="skill-check-app.css">
+</head>
+<body>
+    <div class="header">
+        <a href="../home/index.php">■ホーム</a>
+        <a href="../users/skill.php">&nbsp;■自分のスキル</a>
+        <a>&nbsp;■スキル点数変更</a>
+        <a href="../skills/index.php">&nbsp;■スキル一覧</a>
+    </div>
+    <div class="top">
+        <h1 class="title">@スキルを変更します@</h1>
+    </div>
+    <div class="errmsg">
+        <p><?php echo $errmsg; ?></p>
+    </div>
+    </html>
