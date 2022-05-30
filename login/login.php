3a03f795a8acf7e17160ae697ba5b19f228e72b8
DG-ogura <ogura@designet.co.jp>
2022-05-30 13:35:32 +0900

    add login.php

---
+++ login/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ログイン</title>
+    <link rel="stylesheet" href="skill-check-app.css">
+</head>
+<body>
+<p>■ログイン</p>
+<form action="index.php" method="post">
+<table border=1>
+	<tr>
+		<td>ID</td>
+		<td><input name="input_id" type="text" value=""></td>
+	</tr>
+	<tr>
+		<td>パスワード</td>
+		<td><input name="input_pw" type="text" value=""></td>
+	</tr>
+</table>
+<input type="submit" value="認証">
+<input type="hidden" name="mode" value="login">
+</form>
+</body>
+</html>
+
